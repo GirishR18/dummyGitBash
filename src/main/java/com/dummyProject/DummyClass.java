d09ebcd5cfe996e76f8512d640091663e74f3805
Girish R <43299864+GirishR18@users.noreply.github.com>
2025-10-29 12:18:19 +0530

    Add additional SOP statement in DummyClass

--- src/main/java/com/dummyProject/DummyClass.java
+++ src/main/java/com/dummyProject/DummyClass.java
@@ -5,5 +5,6 @@ public class DummyClass {
 	public void dummy() {
 		System.out.println("Hii");
 		System.out.println("added sop statement");
+		System.out.println("added sop statement from git hub");
 	}
 }
